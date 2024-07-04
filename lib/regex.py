26daf2c62a717caaf6adff147653a01061fc8dc3
Timothy Ryon <tim@timryon.com>
2024-07-03 22:10:04 -0400

    wip 8 of 12 tests pass

--- lib/regex.py
+++ lib/regex.py
@@ -3,7 +3,11 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+# line 7 passes tests 1 and 2
+# name = r"([\w.-]+)\s([\w.-]+)"
+# line 9 passes tests 1 through 4
+# name = r"([\w.-]+)[\s|\S]([\w.-]+)"
+name = r"([\w.-]+)[\s|\S]([\w.-]+)"
 name_regex = re.compile(name)
 
 phone_number = r""
