d0326b3676b514ddc93f82e8e7d4c2fcba1f9534
Timothy Ryon <tim@timryon.com>
2024-07-06 22:48:26 -0400

    finish and pass all tests

--- lib/regex.py
+++ lib/regex.py
@@ -7,11 +7,11 @@ import re
 # name = r"([\w.-]+)\s([\w.-]+)"
 # line 9 passes tests 1 through 4
 # name = r"([\w.-]+)[\s|\S]([\w.-]+)"
-name = r"([\w.-]+)[\s|\S]([\w.-]+)"
+name = r"[A-Z][a-zA-Z'-]+(?:\s[A-Z][a-zA-Z'-]+)?"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"(\d{10})|(\(\d{3}\)\s\d{3}-\d{4})|(\d{3}-\d{3}-\d{4})"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"^[A-Za-z][\w.-]*@[A-Za-z]+\.[A-Za-z]+$"
 email_regex = re.compile(email_address)
